0722f9c6667a5fcc781298617f89b3d2599dc7f5
Charanjit Singh <ckhabra007@gmail.com>
2015-04-26 14:39:17 +0530

    Initial commit

---
+++ package.js
@@ -0,0 +1,38 @@
+Package.describe({
+  name: 'nucleuside:eventsync',
+  version: '0.1.0',
+  summary: 'Sync events across meteor apps',
+  git: 'https://github.com/nucleuside/meteor-eventsync',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom("METEOR@0.9.1");
+
+  api.use(['jquery', 'deps', 'underscore', 'session',
+           'reactive-var',
+           'mrt:moment@2.8.1',
+           'nucleuside:smart-models@0.0.6',]);
+
+  api.addFiles(
+    'both/nucleus_event_model.js',
+    ['client', 'server']);
+
+  api.addFiles([
+    'client/event_manager.js',
+    'client/utils.js',
+    'client/nucleus_file_changes.js',
+    'client/clicks.js',
+    'client/scroll.js',
+    'client/forms.js',
+    'client/form_inputs.js',
+    'client/form_toggles.js',
+    'client/form_submit.js',
+    'client/location.js',
+    'client/login.js',
+  ], 'client');
+
+  api.export(['NucleusEventManager'], ['client']);
+});
+
+Package.onTest(function(api) {});
---
+++ server/permissions.js
@@ -0,0 +1,12 @@
+NucleusEvents.allow({
+    insert: function() {
+        return true;
+    },
+    update: function() {
+        return true;
+    },
+    remove: function() {
+        return true;
+    },
+    fetch: [""]
+});
